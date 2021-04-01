7f673c2a861a38c92fd51850caaab79e8afbc75a
ggg6277 <ggg6277@gmail.com>
2021-04-01 18:34:36 +0900

    first commit

--- myblog/realblog/urls.py
+++ myblog/realblog/urls.py
@@ -4,7 +4,7 @@ from. import views
 
 urlpatterns = [
     path('base/', views.base, name='base'),
-    path('blog/', views.blog, name = 'blog'),
-    path('blog_detail/<int:pk>', views.blog_detail, name = 'blog_detail'),
+    path('blog/', views.blog, name='blog'),
+    path('blog_detail/<int:pk>', views.blog_detail, name='blog_detail'),
     path('base1/', views.base1, name='base1')
 ]
